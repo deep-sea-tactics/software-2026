204bd1a3f87551b2e433be6cef9ee8c0667e309f
Yos-jpg <arandayossef@gmail.com>
2026-03-21 15:01:12 -0400

    class

--- controls.py
+++ controls.py
@@ -8,3 +8,22 @@ joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_coun
 
 #joystick_count = pygame.joystick.get_count()
 
+class Controller:
+    def __init__(self):
+        self.joystick = None
+        if joysticks:
+            self.joystick = joysticks[0]
+            self.joystick.init()
+            print(f"Joystick initialized: {self.joystick.get_name()}")
+        else:
+            print("No joystick found")
+
+    def get_axis(self, axis):
+        if self.joystick:
+            return self.joystick.get_axis(axis)
+        return 0.0
+
+    def get_button(self, button):
+        if self.joystick:
+            return self.joystick.get_button(button)
+        return False
